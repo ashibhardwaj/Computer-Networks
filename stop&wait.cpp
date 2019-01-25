0e599bfabc80b153b2db4aa0dcfbf1d54d72863a
ASHI BHARDWAJ <ashibhardwaj10@gmail.com>
2019-01-25 11:30:31 +0530

    Update stop&wait.cpp

--- stop&wait.cpp
+++ stop&wait.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main()
 {
+    srand(time(0)); // For generating different random number each time.
     int frames, i=1, x;
-    frames=rand()%20+1;
+    frames=rand()%20+1; // Not accepting more than 20 frames for simplicity.
     cout<<"No. of frames = "<<frames<<endl;
     while(frames--)
     {
@@ -13,12 +14,12 @@ int main()
         while(1)
         {
             x=rand()%10;
-            if(x%2==0)
+            if(x%2==0) // Assuming it to be the case when acknowledgement is not received.
             {
                 cout<<"Waiting"<<endl;
                 cout<<"Resending frame "<<i<<endl;
             }
-            else
+            else // Assuming that acknowledgment has been received.
             {
                 cout<<"Acknowledgement for frame "<<i<<endl;
                 break;
@@ -30,3 +31,4 @@ int main()
     return 0;
     
 }
+
